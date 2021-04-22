83722c7e5b61e66e3206be8e1900d4e6272c6716
Senseyka1 <misa.ripta123@gmail.com>
2021-04-22 21:23:50 +0300

    first commit1

---
+++ 5ex1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+    int a[20];
+    int b[20];
+    int c[20];
+    int i,n;
+
+    printf("Введіть розмір массивів: "); scanf("%d",&n);
+    printf("Массив a:\n");
+    for(i=0;i<n;i++)
+    {
+     printf("a[%d]=",i);
+     scanf("%d",&a[i]);
+    }
+
+    printf("Массив b:\n");
+    for(i=0;i<n;i++)
+    {
+     printf("b[%d]=",i);
+     scanf("%d",&b[i]);
+    }
+
+    for(i=0;i<n;i++)
+    {
+     printf("\n");
+     a[i] = a[i] * a[i];
+     printf(" Квадрат елемента  a[%d]  =  %d\n",i,a[i]);
+     b[i] = b[i] * b[i];
+     printf(" Квадрат елемента b[%d] = %d\n",i,b[i]);
+    }
+    printf("---------------Відповідь--------------\n");
+    printf("Сума квадратів елементів a i b:\n");
+
+    for(i=0;i<n;i++)
+    {
+    c[i] = a[i] + b[i];
+     printf("c[%d] = %d\n",i,c[i]);
+     }
+     
+        return 0;
+}
+   
---
+++ 5ex2.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+
+        int i, a[100], b=0;
+        int n, min, sum = 0, index = 0;
+        const int Low=-100 , High= 100;
+
+        char ch;
+        printf("Виберіть свій варіант для вирішення масиву: \n");
+        printf("1: Введіть ваш масив\n");
+        printf("2: Рандом\n");
+        printf("Відповідь: ");
+        
+        scanf("%c",&ch);
+        switch(ch)
+     {
+
+        case '1':
+        printf("\nВведіть розмір массиву: "); scanf("%d",&n);
+        for(i=0;i<n;i++)
+        {
+           printf("a[%d]=",i);
+           scanf("%d",&a[i]);
+           if(a[i] < 0) b++;
+
+        }
+        break;
+
+        case '2':
+        printf("\nВведіть розмір массиву: "); scanf("%d",&n);
+        for(i=0;i<n;i++)
+        {
+        a[i] = Low+rand()%High;
+        printf("a[%d] = %d\n",i,a[i]);
+        if(a[i] < 0) b++;
+        }
+        break;
+      default: printf("\n---------------------!Помилка!-----------------------\n"); break;
+   }
+        printf("\n---------------------Відповідь-----------------------");
+        printf("\nКількість негативних елементів масиву   = %d",b);
+
+        min = a[0];
+        for(i = 0; i < n; i++)
+        {
+        if(a[i] < min)
+           {
+           min = a[i];
+           index = i;
+           index++;
+           }
+        }
+
+       for(i = index; i < n; i++)
+        {
+         sum += a[i];
+        }
+         printf("\n-----------------------------------------------------");
+         printf("\nМинимальний елемент масиву              = %d",min);
+         printf("\n-----------------------------------------------------");
+         printf("\nСума після мінімального елементу масиву = %d",sum);
+
+        return 0;
+}
+ 
