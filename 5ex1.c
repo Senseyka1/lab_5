1063f6a325971029f1790eba1816838010821bae
Senseyka1 <74878665+Senseyka1@users.noreply.github.com>
2021-04-29 16:07:45 +0300

    Update 5ex1.c

--- 5ex1.c
+++ 5ex1.c
@@ -1,43 +1,49 @@
 #include <stdio.h>
 
 int main(){
+    //Оголошення змінних
     int a[20];
     int b[20];
     int c[20];
     int i,n;
 
-    printf("Введіть розмір массивів: "); scanf("%d",&n);
-    printf("Массив a:\n");
-    for(i=0;i<n;i++)
+    //Введення даних
+    printf("\nВведіть розмір массивів: "); 
+    scanf("%d",&n);
+    printf("\nМассив a:\n");
+    for(i=0; i<n; i++)
     {
-     printf("a[%d]=",i);
-     scanf("%d",&a[i]);
+     printf("a[%d]=", i);
+     scanf("%d", &a[i]);
+    }
+    
+    printf("\nМассив b:\n");
+    for(i=0; i<n; i++)
+    {
+     printf("b[%d]=", i);
+     scanf("%d", &b[i]);
     }
 
-    printf("Массив b:\n");
-    for(i=0;i<n;i++)
-    {
-     printf("b[%d]=",i);
-     scanf("%d",&b[i]);
-    }
-
-    for(i=0;i<n;i++)
+    printf("\n--------------------------------------\n");
+    for(i=0; i<n; i++)
     {
      printf("\n");
+
      a[i] = a[i] * a[i];
-     printf(" Квадрат елемента  a[%d]  =  %d\n",i,a[i]);
+     printf(" Квадрат елемента a[%d] = %d\n", i, a[i]);
      b[i] = b[i] * b[i];
-     printf(" Квадрат елемента b[%d] = %d\n",i,b[i]);
+     printf(" Квадрат елемента b[%d] = %d\n", i, b[i]);
     }
-    printf("---------------Відповідь--------------\n");
+
+    // Вивід
+    printf("\n---------------Відповідь--------------\n");
     printf("Сума квадратів елементів a i b:\n");
 
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
     {
     c[i] = a[i] + b[i];
-     printf("c[%d] = %d\n",i,c[i]);
+     printf("c[%d] = %d\n", i, c[i]);
     }
      
-        return 0;
+    return 0;
 }
-   
